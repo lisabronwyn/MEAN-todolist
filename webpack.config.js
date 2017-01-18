7c1194d7dd74a286a0aa4f6a48cdbefca94afd4b
lisabronwyn <e.bronwyn@gmail.com>
2017-01-17 21:38:12 -0800

    made some JSON changes and git ignore

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://127.0.0.1:8080/',
     'webpack/hot/only-dev-server',
-    'bootstrap-loader',
     './src'
   ],
   output: {
@@ -18,46 +17,29 @@ module.exports = {
     extension: ['', '.js']
   },
   module: {
-    loaders: [
-    {
+     [
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel',
         query: {
           presets: ['es2015']
         }
-    },
-    {
-      test: /\.html$/,
-      loader: 'raw'
-    },
-    {
-      test: /\.scss/,
-      loaders: [
-        'style',
-        'css',
-        'autoprefixer?browsers=last 3 versions',
-        'sass?outputStyle=expanded'
-      ]
-    },
-    {
-      test: /.(woff2?|ttf|eot|svg)$/,
-      loader: 'url?limit=10000'
-    },
-    {
-      test: /bootstrap-sass\/assets\/javascripts\//,
-      loader: 'imports?jQuery=jquery'
-    }
+      },
+      {
+        test: /\.html$/,
+        loader: 'raw'
+      }
     ]
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-  devServer: {
-    hot: true,
-    proxy: {
-      '*': 'http://localhost:3000'
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin()
+],
+
+    devServer: {
+      hot: true,
+      proxy: {
+        '*':'http://localhost:3000'
+      }
     }
-  }
-};
+  };
